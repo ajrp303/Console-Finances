772bf9fbe08070d25f191d4f76c35da3b637d5db
unknown <anthonypickard@hotmail.co.uk>
2022-11-16 05:49:08 +0000

    final commit

--- script.js
+++ script.js
@@ -1,31 +1,35 @@
 console.log('Financial Analysis \n\n -------------- \n\n')
 
+// finances.length will = total months due to every month having a profit/loss value
 console.log('Total Months: ' + finances.length);
 
-//To select the profit/losses we need to select all [n][1] elements where n iterates upto the total length of the array//
 TotalProfitLoss = 0;
 
 TotalChange = 0;
 
 ProfitLossArray = [];
 
+// iterate over every array to sum the total profit or loss selecting only the p/l values
 for (index_1 = 0, index_2 = 1; index_1 < finances.length; index_1++, index_2++) {
     TotalProfitLoss = TotalProfitLoss + finances[index_1][1];
+    //iterate over every array to calculate the difference between p/l per month, and form new array with the figures and month
     if (index_2 < finances.length) {
         DifferenceInProfitLoss = finances[index_2][1]-finances[index_1][1];
         ProfitLossArray.push([DifferenceInProfitLoss, finances[index_2][0]]);
+        //take absolute value of the differences in p/l to sum together for meaningful insight
         TotalChange = TotalChange + Math.abs(DifferenceInProfitLoss);        
         }
 } 
 
 console.log('Total: $' + TotalProfitLoss);
-
+//conditional to change aformentioned absolute value to profit or less dependant on value of total profit or loss
 if (TotalProfitLoss < 0) {
     console.log('Average Change: $' + Math.round((TotalChange)*-1/(finances.length-1)))
 } else {
     console.log('Average Change: $' + Math.round(TotalChange/(finances.length-1)));
 }
 
+//sort array on (loss, profit) so results accurate for any data set
 ProfitLossArray.sort(function(a,b) {
     return a[0]-b[0]
 });
